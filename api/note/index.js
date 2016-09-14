1731ce33307584537e9429b1bea50a5f76e16bff
hjl <1617451312@qq.oom>
2016-09-14 19:26:01 +0800

    记事本初始功能实现

---
+++ api/note/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var auth = require('../../auth/auth.service');
+var controller = require('./note.controller')
+var multipart = require('connect-multiparty')
+var router = express.Router();
+
+router.post('/addNote',auth.isAuthenticated(),controller.addNote);
+router.get('/getNoteList',auth.isAuthenticated(),controller.getNoteList);
+router.get('/getNoteCount',auth.isAuthenticated(),controller.getNoteCount);
+router.post('/upload',auth.isAuthenticated(),controller.upload);
+router.put('/:id/updateNote',auth.isAuthenticated(),controller.updateNote);
+router.get('/:id/getNoteDetail',auth.isAuthenticated(),controller.getNoteDetail);
+
+module.exports = router;
---
+++ api/note/note.controller.js
@@ -0,0 +1,95 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Note = mongoose.model('Note');
+var config = require('../../config/env');
+var AVATAR_UPLOAD_FOLDER = path.join(__dirname,'../../public/upload/')
+
+exports.getNoteList = function(req,res,next){
+    var uid = req.user._id;
+    Note.find({uid:uid})
+        .exec().then(function(result){
+            return res.status(200).json({
+                data:result
+            })
+        })
+}
+
+exports.getNoteCount = function(req,res,next){
+    var uid = req.user._id;
+    Note.countAsync({uid:uid}).then(function(result){
+        return res.status(200).json({
+            count:count
+        })
+    })
+        
+}
+
+exports.getNoteDetail = function(req,res,next){
+    var id = req.params.id;
+
+    return Note.findByIdAsync(id).then(function(result){
+        return res.status(200).json({
+            data:result
+        })
+    })
+}
+
+exports.upload = function(req,res){
+    var filename = req.files.files.originalFilename ||  path.basename(req.files.files.ws.path);
+
+//copy file to a public directory
+    var targetPath =AVATAR_UPLOAD_FOLDER + filename;
+
+    
+
+    //copy file
+    fs.createReadStream(req.files.files.path).pipe(fs.createWriteStream(targetPath));
+    //return file url
+    res.json({code: 200, msg: {url: 'http://' + req.headers.host + '/upload' + filename}});
+
+}
+
+exports.addNote = function(req,res,next){
+    var content = req.body.content;
+    var uid = req.user._id;
+    var callTime = req.body.callTime;
+    var error_msg;
+    if(!content && !req.body.images.length){
+        error_msg = '内容不能为空'
+    }
+    if(error_msg){
+        return res.status(422).send({error_msg:error_msg});
+    }
+
+    req.body.uid = req.user._id;
+    return Note.createAsync(req.body)
+        .then(function(result){
+            return res.status(200).json({data:result})
+        })
+
+}
+
+exports.updateNote = function(req,res,next){
+    var id = req.params.id;
+    return Note.findByIdAsync(id)
+        .then(function(note){
+            var note = note;
+            if(req.body.content){
+                note.content = content
+            }
+            if(req.body.images){
+                note.images = images;
+            }
+            if(req.body.callTime){
+                note.callTime = req.body.callTime;
+            }
+            note.saveAsync().then(function(result){
+                return res.status(200).json({
+                    success:true,
+                    data:result
+                })
+            })
+        }).catch(function(err){
+            res.status(500).send(err)
+        })
+}
