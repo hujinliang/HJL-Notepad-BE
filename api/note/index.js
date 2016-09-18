1bd0784f61aeec9c6f9794be668d67f657e7af9e
hujingliang <1617451312@qq.com>
2016-09-18 15:16:34 +0800

    '完善上传功能'

--- api/note/index.js
+++ api/note/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 router.post('/addNote',auth.isAuthenticated(),controller.addNote);
 router.get('/getNoteList',auth.isAuthenticated(),controller.getNoteList);
 router.get('/getNoteCount',auth.isAuthenticated(),controller.getNoteCount);
-router.post('/upload',auth.isAuthenticated(),controller.upload);
+router.post('/upload',controller.upload);
 router.put('/:id/updateNote',auth.isAuthenticated(),controller.updateNote);
 router.get('/:id/getNoteDetail',auth.isAuthenticated(),controller.getNoteDetail);
 router.delete('/:id/deleteNote',auth.isAuthenticated(),controller.deleteNote)
